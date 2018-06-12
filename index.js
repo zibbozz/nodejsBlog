2a73e00992e2a1dbdd6c5ef37d79aeb399d7bcb0
Zibbo <sebastian.wittmann97@web.de>
2018-06-12 16:58:40 +0200

    basic article system

---
+++ index.js
@@ -0,0 +1,37 @@
+let http = require("http");
+let express = require("express");
+let app = express();
+let fs = require("fs");
+let bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/app'));
+
+let server = http.createServer(app);
+
+server.listen(8080);
+
+let entries = [];
+
+let filename = __dirname + '/entries.json';
+
+try{
+	let filedata = fs.readFileSync(filename);
+	entries = JSON.parse(filedata);
+	console.log(entries.length + ' Datensätze eingelesen');
+} catch(err) {
+	console.log("Konnte keine Datensätze einlesen");
+}
+
+app.get('/entries', function(req, res){
+	res.contentType("application/json");
+	res.status(200).send(JSON.stringify(entries));
+});
+
+app.post('/entries', function(req, res){
+	entries.push(req.body);
+	fs.writeFileSync(filename, JSON.stringify(entries));
+	res.contentType("text/html");
+	res.status(201).send("OK");
+})
